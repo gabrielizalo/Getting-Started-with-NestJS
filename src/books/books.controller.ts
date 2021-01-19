4d9d1ae788886f455ebf7770a0cc510be5d85464
Gabriel Porras <ghporras@gmail.com>
2021-01-19 08:09:51 -0500

    feat: added books controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Post, Query} from "@nestjs/common";
+import {BooksService} from "./books.service";
 
-@Controller('books')
-export class BooksController {}
+@Controller("books")
+export class BooksController {
+  constructor(private booksService: BooksService) {
+  }
+
+  @Get()
+  async getBooks() {
+    return await this.booksService.getBooks();
+  }
+
+  @Get(":bookID")
+  async getBook(@Param("bookId") bookId) {
+    return await this.booksService.getBook(bookId);
+  }
+
+  @Post()
+  async addBook(@Body() createBookDTO: CreateBookDTO) {
+    return await this.booksService.addBook(createBookDTO);
+  }
+
+  @Delete()
+  async deleteBook(@Query() query) {
+    return await this.booksService.deleteBook(query.bookId);
+  }
+}
